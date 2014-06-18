a000e8323d96173e5615a861b932cae0f51cfaab
schminitz <gillian@affinitic.be>
2014-06-18 14:13:37 +0200

    Rearrange view to have one line per heb affinitic refs #5828

--- src/gites/pivot/db/scripts/hebergement_view.sql
+++ src/gites/pivot/db/scripts/hebergement_view.sql
@@ -1,3 +1,28 @@
+/* heb_lits_view */
+create or replace view pivot.heb_lits_view
+as
+select pivot.thebergements.codeCGT,
+       sum(pivot.thebergements.nbr_lits_sup) heb_nbr_lits_sup,
+       sum(pivot.thebergements.nbr_lits_simple) heb_nbr_lits_simple,
+       sum(pivot.thebergements.nbr_lits_double) heb_nbr_lits_double,
+       sum(pivot.thebergements.nbr_lits_superpose) heb_nbr_lits_enfant
+from pivot.thebergements
+group by pivot.thebergements.codeCGT;
+
+
+/* ch_lits_view */
+create or replace view pivot.ch_lits_view
+as
+select pivot.tchambres.fk_toffres_codeCGT,
+       sum(pivot.tchambres.nb_lits_sup) ch_nbr_lits_sup,
+       sum(pivot.tchambres.nb_lits_single) ch_nbr_lits_simple,
+       sum(pivot.tchambres.nb_lits_double) ch_nbr_lits_double,
+       sum(pivot.tchambres.nb_lits_bebe) ch_nbr_lits_enfant
+from pivot.tchambres
+group by pivot.tchambres.fk_toffres_codeCGT;
+
+
+/* hebergement_view */
 create or replace view pivot.hebergement_view
 as
 select pivot.toffres.codeCGT,
@@ -32,18 +57,17 @@ select pivot.toffres.codeCGT,
        pivot.toffres.date_creation,
        pivot.toffres.date_modification,
        pivot.toffres.fk_ttypesoffres_id_type_offre,
-       pivot.thebergements.nbr_lits_sup,
-       pivot.thebergements.nbr_lits_simple,
-       pivot.thebergements.nbr_lits_double,
-       pivot.thebergements.nbr_lits_superpose,
-       pivot.tchambres.id_chambre,
-       pivot.tchambres.nb_lits_single,
-       pivot.tchambres.nb_lits_double,
-       pivot.tchambres.nb_lits_sup,
-       pivot.tchambres.nb_lits_bebe
+       heb_lits_view.heb_nbr_lits_sup,
+       heb_lits_view.heb_nbr_lits_simple,
+       heb_lits_view.heb_nbr_lits_double,
+       heb_lits_view.heb_nbr_lits_enfant,
+       ch_lits_view.ch_nbr_lits_sup,
+       ch_lits_view.ch_nbr_lits_simple,
+       ch_lits_view.ch_nbr_lits_double,
+       ch_lits_view.ch_nbr_lits_enfant
 from pivot.toffres
-    left outer join pivot.thebergements
-        on pivot.toffres.codeCGT = pivot.thebergements.codeCGT
-    left outer join pivot.tchambres
-        on pivot.toffres.codeCGT = pivot.tchambres.fk_toffres_codeCGT
+    left outer join pivot.heb_lits_view
+        on pivot.toffres.codeCGT = heb_lits_view.codeCGT
+    left outer join pivot.ch_lits_view
+        on pivot.toffres.codeCGT = ch_lits_view.fk_toffres_codeCGT
 order by codeCGT;
