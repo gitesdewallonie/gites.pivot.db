caa48cbc1d23836b6a74bc7bbe2057fd0d67bd8e
Martin Peeters <martin.peeters@affinitic.be>
2014-07-01 14:37:28 +0200

    Fix database encoding and force unicode for comparisson. refs #5854 (Affinitic)

--- src/gites/pivot/db/tests/test_hebergementview.py
+++ src/gites/pivot/db/tests/test_hebergementview.py
@@ -20,3 +20,8 @@ class TestHebergementView(testing.PivotDBTestCase):
         date = datetime(2014, 06, 1, 0, 0)
         result = HebergementView.get_last_changes(date)
         self.assertEquals(len(result), 1)
+
+    def test_encoding(self):
+        result = HebergementView.first(heb_nom='LA TURBINE')
+        description = u'Maison de caractère dans un site boisé et calme (9 ha)'
+        self.assertEqual(description, result.heb_descriptif_fr)
